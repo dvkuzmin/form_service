7087df08add66d481380e94d1aa08686b7f3c7f5
dvkuzmin <dk_187@mail.ru>
2023-01-08 22:49:47 +0500

    fix doc string, remove envs

--- form_app/src/main.py
+++ form_app/src/main.py
@@ -7,6 +7,7 @@ forms_collection = db['forms_collection']
 
 
 def main():
+    """Функция проверяет наличие записей в БД, если база пустая, то заполняется фикстурами. Возвращает WSGI приложение"""
     db_form = forms_collection.find_one()
     if not db_form:
         insert_documents(forms, forms_collection)
